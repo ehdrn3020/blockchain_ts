afd349237c1a2274f0038a00b3e81d92eb3a9c5a
ehdrn3020 <ehdrnsla3020@naver.com>
2022-11-29 06:59:08 +0900

    20221129 add block chain example code

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,49 @@ interface BlockShape {
     data: string;
 }
 
-class Block implements BlockShape{
-    public hash:string;
+class Block implements BlockShape {
+    public hash: string;
     constructor(
-        public prevHash:string,
-        public height:number,
-        public data:string
-    ){
+        public prevHash: string,
+        public height: number,
+        public data: string
+    ) {
         this.hash = Block.calculateHash(prevHash, height, data);
     }
-    static calculateHash(prevHash:string, height:number, data:string) {
-        const toHash = `${prevHash}`
-        return toHash
+    static calculateHash(prevHash: string, height: number, data: string) {
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
+
+class Blockchain {
+    private blocks: Block[];
+    private getPrevHash() {
+        if (this.blocks.length === 0) return "";
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    constructor() {
+        this.blocks = [];
+    }
+    public addBlock(data: string) {
+        const newBlock = new Block(
+            this.getPrevHash(), 
+            this.blocks.length + 1, 
+            data
+        );
+        this.blocks.push(newBlock);
+    }
+    public getBlocks() {
+        return [...this.blocks];
+    }
+}
+
+// main-------------------------
+const blockchain = new Blockchain();
+blockchain.addBlock("First block");
+blockchain.addBlock("Second block");
+blockchain.addBlock("Third block");
+
+blockchain.getBlocks().push(new Block("xxxx", 1111, "Hackedddd"));
+
+console.log(blockchain.getBlocks());
